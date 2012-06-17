5068353c85d1be25d81e3030753d7a99e6c0aac7
Egon Elbre <egonelbre@gmail.com>
2012-06-17 15:53:03 +0300

    dev: working net

--- js/net.js
+++ js/net.js
@@ -1,58 +1,117 @@
+//TODO: type asserts
+
 function CPU(){
 	this.hotness = 1.0;
 	this.paused = false;
-	this.cur
+	this.next = {};
 }
 
+CPU.methods({
+	schedule : function(node){
+		this.next[node.name] = node;
+	},
+	process : function(){
+		var cur = this.next,
+			keys = Object.keys(cur);
+		this.next = {};
+		keys.map(function(n){ cur[n].compute(); });
+		keys.map(function(n){ cur[n].update(); });
+	}
+});
+
 function Net(){
-	this.nodes = [];
+	this.nodes = {};
+	this.ports = [];
+	this.wires = [];
 	this.CPU = new CPU();
 }
 
 Net.methods({
-	schedule : function(node){
+	hasNode : function(name){
+		return this.nodes[name] != undefined;
+	},
+	newNode : function(name){
+		var node = new Node(this, name);
+		this.nodes[name] = node;
+		this.ports.push(node.input);
+		this.ports.push(node.output);
+		return node;
+	},
+	newWire : function(from, to){
+		var wire = new Wire(this, from.output, to.input);
+		this.wires.push(wire);
+		return wire;
+	},
+	signalNodes : function(){
+		var net = this,
+			keys = Object.keys(this.nodes);
+		keys.map(function(name){
+			net.nodes[name].signal(this);
+		});
+	},
+	print : function(){
+		var net = this,
+			status = "===\n",
+			keys = Object.keys(this.nodes);
+		keys.map(function(name){
+			var node = net.nodes[name];
+			status += name + ":" + node.value + ":" + node.bias + "\n";
+		});	
 
+		console.log(status);
 	}
 });
 
-function Node(net){
+// Node drives computation
+function Node(net, name){
 	this.net = net;
-	this.input = new Port(this, "float", "in");
-	this.output = new Port(this, "float", "out");
+	this.name = name;
+	this.input = new Port(this.net, this);
+	this.output = new Port(this.net, this);
 	this.bias = 0;
-	this.value = null;
+	this.value = 0;
 }
 
 Node.methods({
-	signal : function(sender){
-		this.net.schedule(this);
+	// set the node and output port value
+	set : function(value){
+		this.value = value;
+		this.output.set(this.value);
 	},
+	// schedule an update for this node
+	signal : function(sender){
+		this.net.CPU.schedule(this);
+	},
+	// compute the next value for the output port
 	compute : function(){
 		var inputs = this.input.collect();
-		var value = 0;
+		var value = this.bias;
 		for(var i = 0; i < inputs.length; i += 1){
-			var input = inputs[i];
-			value += input.data * input.modifier;
+			value += inputs[i];
 		}
+
+		value = value > 0.0 ? 1.0 : value < 0.0 ? -1.0 : 0.0;
 		// aggregate inputs
 		// apply function
 		this.value = value;
 	},
+	// set the output port to the target value
 	update : function(){
-		output.set(this.value);
+		this.output.set(this.value);
 	}
 });
 
-function Port(owner, typ){
-	this.typ = typ;
+// Port is basically a multiplexer for wires
+function Port(net, owner){
+	this.net = net;
 	this.owner = owner;
 	this.wires = [];
 	this.value = null;
 }
 
 Port.methods({
-	/* set the port value
-	    signal connected ports if the value changed */
+	// set the port value
+	// signal connected ports
 	set : function(value){
 		//TODO: check value type
 		if(this.value == value)
@@ -63,9 +122,11 @@ Port.methods({
 			w.signal(this);
 		}
 	},
+	// get the current value
 	get : function(){
 		return this.value;
 	},
+	// collect information from the wires
 	collect : function(){
 		var result = [];
 		for(var i = 0; i < this.wires.length; i += 1){
@@ -74,35 +135,49 @@ Port.methods({
 		}
 		return result;
 	},
+	// signal the owner node
 	signal : function(sender){
-		this.owner.signal(this);
+		this.owner.signal(sender);
 	}
 });
 
-function Wire(from, to, modifier){
+// Wire is basically a signal transimtter
+function Wire(net, from, to){
+	this.net = net;
 	this.from = from;
 	this.to = to;
-	this.modifier = modifier;
+	this.modifier = 1.0;
+
+	this.from.wires.push(this);
+	this.to.wires.push(this);
 }
 
 Wire.methods({
+	// signal the target port
 	signal: function(sender){
 		this.to.signal(sender);
 	},
+	// get the value from source port
 	get: function(){
 		var data = this.from.get();
 		return this.func(data, this.modifier);
 	},
+	// apply the modifier to the data
 	func : function(data, modifier){
-		//TODO: generics
+		//TODO: multiple dispatch based on data/modifier type
 		return data * modifier;
 	}	
 });
 
-function connectNodes(inp, out, data){
-	if(inp.output.typ != out.output.typ)
-		throw "Types do not match!";
-	var w = new Wire(inp.output, out.input, data);
-	inp.output.wires.add(w);
-	out.input.wires.add(w);
-}
+
+
+net = new Net();
+x = net.newNode("x")
+y = net.newNode("y")
+x$y = net.newWire(x, y);
+y$x = net.newWire(y, x);
+
+x.set(1.0);
+y.set(-1.0);
+
+net.signalNodes();
